16a4c649b07225d20f9154cb9f9392f8eb275ae8
Amoodaa <amoodaa@gmail.com>
2019-07-09 16:38:45 +0300

    added tests for markTodo added done property for every test relates #17

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const test = require('tape');
 const todoFunctions = require('../JS/logic');
 
 const state = [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
+    { id: -3, description: 'first todo', done: false},
+    { id: -2, description: 'second todo', done: false},
+    { id: -1, description: 'third todo', done: false},
   ]; 
 
 test("example test", t => {
@@ -13,14 +13,19 @@ test("example test", t => {
 });
 
 test("testing for delete", (t)=> {
-    const expected1 = [ { id: -3, description: 'first todo' },{ id: -2, description: 'second todo' }];
+    const expected1 = [ 
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false}
+    ];
     const actual1 = todoFunctions.deleteTodo(state, -1);
     let stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
     const newState = [
-        { id: -3, description: 'first todo' },
-        { id: -2, description: 'second todo' }
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false}
     ]
-    const expected2 = [ { id: -3, description: 'first todo' }];
+    const expected2 = [ 
+        { id: -3, description: 'first todo', done: false}
+    ];
     const actual2 = todoFunctions.deleteTodo(newState, -2);
     
     t.deepEqual(actual1, expected1, 'should delete todo.id');
@@ -32,30 +37,55 @@ test("testing for delete", (t)=> {
 
 test("Test of the state array with addTodo function" , (t)=> {
     const expected1 = [
-        { id: -3, description: 'first todo' },
-        { id: -2, description: 'second todo' },
-        { id: -1, description: 'third todo' },
-        { id: 1, description: 'how are you' , done: false},
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false},
+        { id: -1, description: 'third todo', done: false},
+        { id: 1, description: 'how are you', done: false},
     ]
     const expected2 = [
-        { id: -3, description: 'first todo' },
-        { id: -2, description: 'second todo' },
-        { id: -1, description: 'third todo' },
-        { id: 2, description: 'thank you' , done: false},
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false},
+        { id: -1, description: 'third todo', done: false},
+        { id: 2, description: 'thank you', done: false},
     ]
     const expected3 = [
-        { id: -3, description: 'first todo' },
-        { id: -2, description: 'second todo' },
-        { id: -1, description: 'third todo' },
-        { id: 3, description: 'you are welcome' , done: false},
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false},
+        { id: -1, description: 'third todo', done: false},
+        { id: 3, description: 'you are welcome', done: false},
     ]
 
     const stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
     
     t.deepEqual(todoFunctions.addTodo(state , 'how are you') , expected1 , "first add test");
-    t.deepEqual(state, stateBeforeTest,"add function didnt change on the input(pure function)")
+    t.deepEqual(state, stateBeforeTest,"add function didnt change on the input(pure function)");
     t.deepEqual(todoFunctions.addTodo(state , 'thank you') , expected2 , "second add test");
     t.deepEqual(todoFunctions.addTodo(state , 'you are welcome') , expected3 , "third add test");
 
+    t.end();
+});
+
+test("testing for markTodo",(t)=>{
+    const expected1 = [
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false},
+        { id: -1, description: 'third todo', done: true},
+    ]; 
+    const expected2 = [
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: true},
+        { id: -1, description: 'third todo', done: false},
+    ]; 
+    const expected3 = [
+        { id: -3, description: 'first todo', done: true},
+        { id: -2, description: 'second todo', done: false},
+        { id: -1, description: 'third todo', done: false},
+    ]; 
+    const stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
+    
+    t.deepEqual(todoFunctions.markTodo(state, -1),expected1,"marking id -1 test");
+    t.deepEqual(state, stateBeforeTest,"mark function didnt change on the input(pure function)");
+    t.deepEqual(todoFunctions.markTodo(state, -2),expected2,"marking id -2 test");
+    t.deepEqual(todoFunctions.markTodo(state, -3),expected3,"marking id -3 test");
     t.end();
 })
