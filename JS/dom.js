590d97ec0af59ef0749165ba9be1046bc44ae32f
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-07-10 16:25:30 +0300

    added blur event for input, added classes in dom.js for every child element that has style relates #47 #49

--- JS/dom.js
+++ JS/dom.js
@@ -2,81 +2,37 @@
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 (function() {
-    // This is the dom node where we will keep our todo
-    var container = document.getElementById('todo-container');
-    var addTodoForm = document.getElementById('add-todo');
-  
-    var state = [
-      { id: -3, description: 'first todo' },
-      { id: -2, description: 'second todo' },
-      { id: -1, description: 'third todo' },
-    ]; // this is our initial todoList
-  
-    // This function takes a todo, it returns the DOM node representing that todo
-    var createTodoNode = function(todo) {
-      var todoNode = document.createElement('li');
-      // you will need to use addEventListener
-  
-      // add span holding description
-      const descDiv   = document.createElement('DIV');
-      const descSpan  = document.createElement('SPAN');
+  // This is the dom node where we will keep our todo
+  var container = document.getElementById('todo-container');
+  var addTodoForm = document.getElementById('add-todo');
 
-      const iconDiv   = document.createElement('DIV');
-      const checkIcon = document.createElement('I');
-      const delIcon   = document.createElement('I');
+  var state = [
+    { id: -3, description: 'first todo' },
+    { id: -2, description: 'second todo' },
+    { id: -1, description: 'third todo' },
+  ]; // this is our initial todoList
 
-      // add classes for css
-      todoNode.className = 'task';
-      descDiv.className = 'desc-div';
-      iconDiv.className = 'icon-div'
-      checkIcon.className = 'far fa-check-square';
-      delIcon.className = 'fas fa-trash-alt';
+  // This function takes a todo, it returns the DOM node representing that todo
+  var createTodoNode = function(todo) {
+    var todoNode = document.createElement('li');
+    // you will need to use addEventListener
 
-      //Apend child
-      descDiv.appendChild(descSpan);
-      iconDiv.appendChild(checkIcon);
-      iconDiv.appendChild(delIcon);
+    // add span holding description
+    const descDiv   = document.createElement('DIV');
+    const descSpan  = document.createElement('SPAN');
 
-      todoNode.appendChild(descDiv);
-      todoNode.appendChild(iconDiv);
-
-      //Fill information by todo object
-      descSpan.textContent = todo.description;
-  
-      delIcon.addEventListener('click', function(event) {
-        var newState = todoFunctions.deleteTodo(state, todo.id);
-        update(newState);
-      });
-      /* mark btn */
-      checkIcon.addEventListener('click', function(event) {
-        var newState = todoFunctions.markTodo(state, todo.id);
-        update(newState);
-      });
-      return todoNode;
-    };
-  
-    // bind create todo form
-    if (addTodoForm) {
-      addTodoForm.addEventListener('submit', function(event) {
-        // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-        // what does event.preventDefault do?
-        // what is inside event.target?
-        event.preventDefault();
-        const inputText = document.querySelector('input[name=description]').value;
-        
-        if(inputText){
-
-          console.log(inputText);
-          
-          var newState = todoFunctions.addTodo(state , inputText); // ?? change this!
-          update(newState);
-        }
-  
-        // hint: todoFunctions.addTodo
-        //Reset Value
-        document.querySelector('input[name=description]').value = "";
-
-      });
+    const iconDiv   = document.createElement('DIV');
+    const checkIcon = document.createElement('I');
+    const delIcon   = document.createElement('I');
+    descDiv.classList.add('desc-div')
+    iconDiv.classList.add('icon-div');
+    // add classes for css
+    if(todo.done){
+      checkIcon.className = "far fa-check-square";
+      checkIcon.style.color = 'green'
+    }else{
+      checkIcon.className = "far fa-square";
+      checkIcon.style.color = 'red'
     }
 
     delIcon.className = 'fas fa-trash-alt';
@@ -107,6 +63,7 @@
       var inputNew = document.createElement('input');
       descDiv.removeChild(descSpan);
       descDiv.appendChild(inputNew);
+      inputNew.classList.add('editInput')
       inputNew.focus();
       inputNew.addEventListener('blur',function(event){
         descDiv.removeChild(inputNew);
