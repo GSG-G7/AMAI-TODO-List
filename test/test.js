0f9cf058178da09450fd2168bd2d3b31d319df53
AlaaTaima <alaa.ktaima@gmail.com>
2019-07-11 10:04:18 +0300

    re-arranged variables declarations #66

--- test/test.js
+++ test/test.js
@@ -13,25 +13,27 @@ test("example test", t => {
 });
 
 test("testing for delete", (t)=> {
+    const stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
     const expected1 = [ 
         { id: -3, description: 'first todo', done: false},
         { id: -2, description: 'second todo', done: false}
     ];
-    const actual1 = todoFunctions.deleteTodo(state, -1);
-    let stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
+    const actual1 = todoFunctions.deleteTodo(stateBeforeTest, -1);
     const newState = [
         { id: -3, description: 'first todo', done: false},
         { id: -2, description: 'second todo', done: false}
     ]
+    let newStatePure = todoFunctions.cloneArrayOfObjects(newState);
+
     const expected2 = [ 
         { id: -3, description: 'first todo', done: false}
     ];
-    const actual2 = todoFunctions.deleteTodo(newState, -2);
+    const actual2 = todoFunctions.deleteTodo(newStatePure, -2);
     
     t.deepEqual(actual1, expected1, 'should delete todo.id');
     t.deepEqual(state, stateBeforeTest,"delete function must be pure");
     t.deepEqual(actual2, expected2, 'should delete todo.id');
-    t.deepEqual(state, stateBeforeTest,"delete function must be pure");
+    t.deepEqual(newState, newStatePure,"delete function must be pure");
     t.end();
 });
 
@@ -57,10 +59,10 @@ test("Test of the state array with addTodo function" , (t)=> {
 
     const stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
     
-    t.deepEqual(todoFunctions.addTodo(state , 'how are you') , expected1 , "first add test");
+    t.deepEqual(todoFunctions.addTodo(stateBeforeTest , 'how are you') , expected1 , "first add test");
     t.deepEqual(state, stateBeforeTest,"add function didnt change on the input(pure function)");
-    t.deepEqual(todoFunctions.addTodo(state , 'thank you') , expected2 , "second add test");
-    t.deepEqual(todoFunctions.addTodo(state , 'you are welcome') , expected3 , "third add test");
+    t.deepEqual(todoFunctions.addTodo(stateBeforeTest , 'thank you') , expected2 , "second add test");
+    t.deepEqual(todoFunctions.addTodo(stateBeforeTest , 'you are welcome') , expected3 , "third add test");
 
     t.end();
 });
@@ -83,10 +85,10 @@ test("testing for markTodo",(t)=>{
     ]; 
     const stateBeforeTest = todoFunctions.cloneArrayOfObjects(state);
     
-    t.deepEqual(todoFunctions.markTodo(state, -1),expected1,"marking id -1 test");
+    t.deepEqual(todoFunctions.markTodo(stateBeforeTest, -1),expected1,"marking id -1 test");
     t.deepEqual(state, stateBeforeTest,"mark function didnt change on the input(pure function)");
-    t.deepEqual(todoFunctions.markTodo(state, -2),expected2,"marking id -2 test");
-    t.deepEqual(todoFunctions.markTodo(state, -3),expected3,"marking id -3 test");
+    t.deepEqual(todoFunctions.markTodo(stateBeforeTest, -2),expected2,"marking id -2 test");
+    t.deepEqual(todoFunctions.markTodo(stateBeforeTest, -3),expected3,"marking id -3 test");
     t.end();
 })
 
@@ -139,10 +141,10 @@ test("testing for sort function", t=>{
         ];
 
 
-    const actual1 = todoFunctions.sortTodos(unSort1,todoFunctions.sortFunction);
+    const unSort1Pure = todoFunctions.cloneArrayOfObjects(unSort1);
+    const actual1 = todoFunctions.sortTodos(unSort1Pure,todoFunctions.sortFunction);
     const actual2 = todoFunctions.sortTodos(unSort2,todoFunctions.sortFunction);
     const actual3 = todoFunctions.sortTodos(unSort3,todoFunctions.sortFunction);
-    const unSort1Pure = todoFunctions.cloneArrayOfObjects(unSort1);
 
     t.deepEqual(actual1,expected1,"array should be sorted");
     t.deepEqual(unSort1,unSort1Pure,"sort function didnt change on the input(pure function)");
